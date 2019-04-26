66d0526cb6aebc1d429d25e5c4d792d391d6f0b7
guojia <jia1324981330@163.com>
2019-04-26 15:36:31 +0800

    abc

--- Project/src/main/java/springmvc/Test1.java
+++ Project/src/main/java/springmvc/Test1.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 public class Test1 {
 
 	public static void main(String[] args) throws IOException {
+		System.out.println();
 		File file = new File ("D:\\userinfo.txt");
 		FileWriter fw= new FileWriter(file,true);
 		BufferedWriter bw = new BufferedWriter(fw);
